1ec4913c32e876181e8071232dcd590d94470a4d
ruska- <ruska.beridze@gmail.com>
2023-12-07 15:15:58 +0400

    open/close date picker on button click

---
+++ script.js
@@ -0,0 +1,6 @@
+const datePickerButton = document.querySelector(".date-picker-button");
+const datePicker = document.querySelector(".date-picker");
+
+datePickerButton.addEventListener("click", (e) => {
+  datePicker.classList.toggle("show");
+});

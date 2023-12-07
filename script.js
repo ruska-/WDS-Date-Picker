bd1b6cb4b2966ce7f1b908f7b963239e54fd2b44
ruska- <ruska.beridze@gmail.com>
2023-12-07 15:43:59 +0400

    added previous and next buttons functionality

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-import { format, fromUnixTime, getUnixTime } from "date-fns";
+import {
+  addMonths,
+  format,
+  fromUnixTime,
+  getUnixTime,
+  subMonths,
+} from "date-fns";
 
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
 const datePickerHeaderText = document.querySelector(".current-month");
+
+const prevMonthButton = document.querySelector(".prev-month-button");
+const nextMonthButton = document.querySelector(".next-month-button");
 
 datePickerButton.addEventListener("click", (e) => {
   datePicker.classList.toggle("show");
@@ -13,12 +22,30 @@ datePickerButton.addEventListener("click", (e) => {
 function setDate(date) {
   datePickerButton.innerText = format(date, "MMMM do, uuuu");
   datePickerButton.dataset.selectedDate = getUnixTime(date);
-
   datePickerHeaderText.innerText = format(date, "MMMM");
 }
 
-setDate(new Date());
-
-function setupDatePicker(date) {
-  datePickerHeaderText.innerText = format(date, "MMMM - uuuu");
+function setupDatePicker(selectedDate) {
+  datePickerHeaderText.innerText = format(selectedDate, "MMMM - uuuu");
+  setupMonthButtons(selectedDate);
 }
+
+function setupMonthButtons(selectedDate) {
+  nextMonthButton.addEventListener(
+    "click",
+    () => {
+      setupDatePicker(addMonths(selectedDate, 1));
+    },
+    { once: true }
+  );
+
+  prevMonthButton.addEventListener(
+    "click",
+    () => {
+      setupDatePicker(subMonths(selectedDate, 1));
+    },
+    { once: true }
+  );
+}
+
+setDate(new Date());

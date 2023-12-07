7c8040ac9f35fb9a2687819dcf4c0f8f014f6e1a
ruska- <ruska.beridze@gmail.com>
2023-12-07 15:32:30 +0400

    date picker header month

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-import { format } from "date-fns";
+import { format, fromUnixTime, getUnixTime } from "date-fns";
 
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
+const datePickerHeaderText = document.querySelector(".current-month");
 
 datePickerButton.addEventListener("click", (e) => {
   datePicker.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  setupDatePicker(selectedDate);
 });
 
 function setDate(date) {
   datePickerButton.innerText = format(date, "MMMM do, uuuu");
+  datePickerButton.dataset.selectedDate = getUnixTime(date);
+
+  datePickerHeaderText.innerText = format(date, "MMMM");
 }
 
 setDate(new Date());
+
+function setupDatePicker(date) {
+  datePickerHeaderText.innerText = format(date, "MMMM - uuuu");
+}

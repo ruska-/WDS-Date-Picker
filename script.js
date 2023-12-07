344156264f72d97bb5f937add3afb99d9b53fa68
ruska- <ruska.beridze@gmail.com>
2023-12-07 15:23:42 +0400

    added current date and function for setting date on a button

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+import { format } from "date-fns";
+
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
 
 datePickerButton.addEventListener("click", (e) => {
   datePicker.classList.toggle("show");
 });
+
+function setDate(date) {
+  datePickerButton.innerText = format(date, "MMMM do, uuuu");
+}
+
+setDate(new Date());
